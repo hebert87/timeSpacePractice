8ab5c848afa651a339d3f3b51746ffb0dcd0b6f8
JPbooya <perezjcarlos81@gmail.com>
2026-04-22 12:21:50 -0700

    finished problem 8

--- src/Practice.java
+++ src/Practice.java
@@ -91,9 +91,9 @@ public class Practice {
 
   // Assume all strings in strs are of length s
   // Hint: charAt is an O(1) operation in both time and space
-  // Time Complexity:
-  // Space Complexity:
-  // Remember to define your variables!
+  // Time Complexity: O(n*m)
+  // Space Complexity: O(n*m) 
+  // Remember to define your variables! n = strs.length m = str.legnth()
   public static List<Character> concatStrings(List<String> strs) {
     List<Character> concat = new ArrayList<>();
     for(String str : strs) {
