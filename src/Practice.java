b27d58d235b9480b13d31aef9dee6bb8f3cb4528
Jessica H <jessicahebert75@gmail.com>
2026-04-22 13:47:19 -0700

    added Time Space Complexity

--- src/Practice.java
+++ src/Practice.java
@@ -125,8 +125,8 @@ public class Practice {
    * 
    * Once you finish, WRITE TESTS FOR IT in PracticeTest.java
    * 
-   * Time Complexity: 
-   * Space Complexity: 
+   * Time Complexity: O(n)
+   * Space Complexity: O(n) n= length of nums
    * 
    * @param nums An array of integers
    * @return the integer that shows up most commonly
